1d49185f25d8e19c10148d37e5b0bdf797c3b750
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-08-14 16:53:43 +0700

    Update Cau1.asm

--- Thuc_hanh_else/Khoi_dong/Cau1.asm
+++ Thuc_hanh_else/Khoi_dong/Cau1.asm
@@ -1,13 +1,13 @@
 section	.text
-	global _start       
-_start:                     
-	mov	edx, len    
-	mov	ecx, msg    
-	mov	ebx, 1	    
-	mov	eax, 4	   
-	int	0x80        
-	mov	eax, 1	    
-	int	0x80        
+	global _start       ;khai bao de su dung gcc
+_start:                     ;tell linker entry point
+	mov	edx, len    ;message length
+	mov	ecx, msg    ;message to write
+	mov	ebx, 1	    ;file descriptor (stdout)
+	mov	eax, 4	    ;system call number (sys_write)
+	int	0x80        ;call kernel
+	mov	eax, 1	    ;system call number (sys_exit)
+	int	0x80        ;call kernel
 
 section	.data
 
